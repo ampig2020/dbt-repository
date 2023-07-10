cc8eab9f57b9cac6105e08ba47bea058abe24130
Isidro P Ampig III <ampig2020@gmail.com>
2023-07-10 02:35:57 +0000

    Jinja Basics

---
+++ jinja_code/first_jinja_code.sql
@@ -0,0 +1,12 @@
+{% set my_cool_string = 'Wow cool'  %}
+{% set my_second_cool_string = 'Im so Cool'%}
+{% set my_third_cool_string = 100 %}
+
+{{my_cool_string}} {{my_second_cool_string}}
+
+{% set_my_array = ['lion', 'tiger', 'dog', 'seal'] %}
+
+{{set_my_array[0]}}
+{{set_my_array[1]}}
+{{set_my_array[2]}}
+{{set_my_array[3]}}
