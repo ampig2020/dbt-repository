3db6efcc234c9717be82c62d3d70b2d1dc003702
Isidro P Ampig III <ampig2020@gmail.com>
2023-11-09 03:31:06 +0000

    Staging

--- models/marts/Apple_Shop/staging/Apple_Shop/stg_apple_shop_customers_tpch_sf100.sql
+++ models/marts/Apple_Shop/staging/Apple_Shop/stg_apple_shop_customers_tpch_sf100.sql
@@ -1,17 +1,17 @@
-{{ config(materialized='table') }}
+-- {{ config(materialized='table') }}
 
-WITH source_customer AS (
-    SELECT
-        C_ACCTBAL,
-        C_ADDRESS,
-        C_COMMENT,
-        C_CUSTKEY,
-        C_MKTSEGMENT,
-        C_NAME,
-        C_NATIONKEY,
-        C_PHONE
-    FROM {{ source('tpch_sf100', 'customer') }}
-)
+-- WITH source_customer AS (
+--     SELECT
+--         C_ACCTBAL,
+--         C_ADDRESS,
+--         C_COMMENT,
+--         C_CUSTKEY,
+--         C_MKTSEGMENT,
+--         C_NAME,
+--         C_NATIONKEY,
+--         C_PHONE
+--     FROM {{ source('tpch_sf100', 'customer') }}
+-- )
 
-SELECT *
-FROM source_customer;
+-- SELECT *
+-- FROM source_customer;
