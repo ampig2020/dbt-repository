8e54c32a5d36704236194c615d4258ae21e110ef
Isidro P Ampig III <ampig2020@gmail.com>
2024-04-03 09:11:36 +0000

    Flattening Dl0 table JSON data and materialized it in DL1 table

---
+++ models/marts/APP/STG/PROD_DL1_CHIEF_FINANCIAL_OFFICE_RQ.sql
@@ -0,0 +1,25 @@
+-- models/flatten_categories.sql
+
+{{ config(
+    materialized='view',
+    schema='DL1_DATABASE.PROD_DL1_CHIEF_FINANCIAL_OFFICE_RQ',
+    database='DL1_DATABASE'
+) }}
+
+WITH source_data AS (
+    SELECT
+        *
+    FROM
+        {{ source('DL0_DATABASE', 'PROD_DL0_CHIEF_FINANCIAL_OFFICE_RQ_raw__stream_categories') }}
+)
+
+SELECT
+    _airbyte_raw_id,
+    _airbyte_extracted_at,
+    _airbyte_loaded_at,
+    COALESCE(
+        TRIM(CAST(JSON_VALUE(__airbyte_data, '$.name') AS VARCHAR)),
+        CAST(NULL AS VARCHAR)
+    ) AS category_name
+FROM
+    source_data
